7b6fbad9953a275ef5ed33bc1a0e529b156cce20
ShinYongSUng <newxm@naver.com>
2020-06-13 12:05:36 +0900

    로그인 서버 요청 응답 확인
     => 향후 응답 내용 분석해서 화면에서 활용할 준비

--- app/src/main/java/my/shin/apipractice_20200613/LoginActivity.kt
+++ app/src/main/java/my/shin/apipractice_20200613/LoginActivity.kt
@@ -2,6 +2,7 @@ package my.shin.apipractice_20200613
 
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import my.shin.utils.SeverUtil
 
 class LoginActivity : BaseAcitivity() {
 
@@ -21,6 +22,9 @@ class LoginActivity : BaseAcitivity() {
             val inputPw = pwEdt.text.toString()
 
 //            실제로 서버에 두개의 변수를 전달해서 로그인 시도
+//            별개의 클래스 (ServerUtil)에 서버 요청 기능을 만들고, 화면에서는 이를 사용.
+
+            SeverUtil.postRequestLogin(mcontext, inputEmail, inputPw,null)
 
         }
 
