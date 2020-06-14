ba195d9e8be7d2f49af98f9c735e7212dd124818
ShinYongSUng <newxm@naver.com>
2020-06-14 11:42:30 +0900

    3초 후에 토큰 검사 => 성공시 메인화면 진입

--- app/src/main/java/my/shin/apipractice_20200613/SplashActivity.kt
+++ app/src/main/java/my/shin/apipractice_20200613/SplashActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import my.shin.utils.ContextUtill
+import my.shin.utils.ServerUtil
+import org.json.JSONObject
 
 class SplashActivity : BaseAcitivity() {
 
@@ -23,12 +26,43 @@ class SplashActivity : BaseAcitivity() {
         val  myHandler = Handler()
 
         myHandler.postDelayed({
-            val  myIntent = Intent(mcontext, LoginActivity::class.java)
-            startActivity(myIntent)
-
-            finish()
 //            일정 시간이 지난 뒤에 실행할 내용
-        }, 3000)
+//            자동로그인? => 안한다면 무조건 로그인 호마ㅕㄴ으로
+//            한다고하면? => 저장된 토큰이 있나? => 있다면 => 서버에서 사용자 정보를 가져오는가?
+//            정보를 가져오기까지 성공하면 => 메인화면(토큰의 유효성을 검정)
+
+            if (ContextUtill.isAutoLogin(mcontext)) {
+                if (ContextUtill.getUserToken(mcontext) !=""){
+                    ServerUtil.getRequestMyInfo(mcontext, object : ServerUtil.JsonResponseHandler {
+                        override fun onResponse(json: JSONObject) {
+
+                            val code = json.getInt("code")
+                            if (code ==200) {
+                                val  myIntent = Intent(mcontext, MainActivity::class.java)
+                                startActivity(myIntent)
+                                finish()
+
+                            }
+                            else {
+                                val  myIntent = Intent(mcontext, LoginActivity::class.java)
+                                startActivity(myIntent)
+                                finish()
+
+                            }
+
+                        }
+                    })
+                }
+
+            }
+            else {
+                val  myIntent = Intent(mcontext, LoginActivity::class.java)
+                startActivity(myIntent)
+                finish()
+            }
+
+
+        },3000)
 
 
 
