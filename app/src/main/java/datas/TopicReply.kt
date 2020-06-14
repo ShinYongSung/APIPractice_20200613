1fbf2ff1f2c022695488457b3aa8e81a7bc6e7a7
ShinYongSUng <newxm@naver.com>
2020-06-14 16:18:39 +0900

    댓글 파싱 코드 추가중  => 작성자를 추가로 파싱해야하는 구조

--- app/src/main/java/datas/TopicReply.kt
+++ app/src/main/java/datas/TopicReply.kt
@@ -6,7 +6,17 @@ class TopicReply {
 
     companion object {
         fun getTopicReplyFromJson(json : JSONObject) : TopicReply {
-            val tr = TopicReply
+            val tr = TopicReply()
+            
+            tr.id = json.getInt("id")
+            tr.content = json.getString("content")
+            tr.topicId = json.getInt("topic_id")
+            tr.sideId = json.getInt("side_id")
+            tr.userId = json.getInt("user_id")
+//            tr.user =??  // 의견 JSON의 항목중 user JSONOBJECT를 => ser 클래스에 전달
+//            user 클래스가 Json을 받아서 => User로 변환해서 리턴. 그걸 tr.user에 대입
+
+
             return tr
         }
     }
