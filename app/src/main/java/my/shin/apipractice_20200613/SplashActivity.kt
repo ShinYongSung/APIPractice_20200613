8184356181255f282b8c87e3f95bb170b641d30b
ShinYongSUng <newxm@naver.com>
2020-06-14 11:24:35 +0900

    3초 후에 로그인 화면 이동
     - Handler 클래스 사용

--- app/src/main/java/my/shin/apipractice_20200613/SplashActivity.kt
+++ app/src/main/java/my/shin/apipractice_20200613/SplashActivity.kt
@@ -1,7 +1,9 @@
 package my.shin.apipractice_20200613
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseAcitivity() {
 
@@ -15,6 +17,21 @@ class SplashActivity : BaseAcitivity() {
     }
 
     override fun setvalues() {
+
+//        3초 뒤에 다음 화면으로 이동
+
+        val  myHandler = Handler()
+
+        myHandler.postDelayed({
+            val  myIntent = Intent(mcontext, LoginActivity::class.java)
+            startActivity(myIntent)
+
+//            일정 시간이 지난 뒤에 실행할 내용
+        }, 3000)
+
+        finish()
+
+
     }
 
 
