1013ae97cdf53cfed44b2a1182ff68d1bb7c1e73
ShinYongSUng <newxm@naver.com>
2020-06-14 11:49:46 +0900

    로그아웃 처리  - 저장되있던 토큰을 빈칸으로 되돌리기

--- app/src/main/java/my/shin/apipractice_20200613/MainActivity.kt
+++ app/src/main/java/my/shin/apipractice_20200613/MainActivity.kt
@@ -1,7 +1,11 @@
 package my.shin.apipractice_20200613
 
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import my.shin.utils.ContextUtill
 import my.shin.utils.ServerUtil
 import org.json.JSONObject
 
@@ -14,6 +18,24 @@ class MainActivity : BaseAcitivity() {
     }
 
     override fun setupevents() {
+        logoutBtn.setOnClickListener {
+//            정말 로그아웃 할건지? 확인받자
+            val alert = AlertDialog.Builder(mcontext)
+//            alertdialog는 x로 시작하는 android로
+            alert.setTitle("로그아웃 확인")
+            alert.setMessage("정말 로그아웃 하시겠습니까?")
+            alert.setPositiveButton("확인", DialogInterface.OnClickListener { dialog, which ->
+//                실제로 로그아웃 하는 방법 => 저장된 토크을 삭제 (빈칸으로 변경
+                ContextUtill.setUserToken(mcontext, "")
+            })
+            alert.setNegativeButton("취소",null)
+            alert.show()
+
+            val myIntent = Intent(mcontext, LoginActivity::class.java)
+            startActivity(myIntent)
+
+            finish()
+        }
     }
 
     override fun setvalues() {
