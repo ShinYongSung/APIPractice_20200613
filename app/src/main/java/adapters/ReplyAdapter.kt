94f509787e859f37d980bfebaa21220f2fc34566
ShinYongSUng <newxm@naver.com>
2020-06-20 13:35:41 +0900

    실제 좋아요 / 싫어요 / 답글 갯수 출력

--- app/src/main/java/adapters/ReplyAdapter.kt
+++ app/src/main/java/adapters/ReplyAdapter.kt
@@ -3,6 +3,7 @@ package adapters
 import android.icu.text.Transliterator
 import android.view.LayoutInflater
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
 import datas.TopicReply
 import my.shin.apipractice_20200613.R
@@ -26,13 +27,24 @@ class ReplyAdapter(val mContext, val resId:Int, val mList:List<TopicReply>): Arr
 
         val writerNicknameTxt = row.findviewById<TextView>(R.id.writerNickNameTxt)
         val contentText = row.findViewById <TextView>(R.id.contentTxt)
+        val replyBtn = row.findViewById <Button>(R.id.replyBtn)
+        val likeBtn = row.findViewById <Button>(R.id.likeBtn)
+        val dislikeBtn = row.findViewById <Button>(R.id.dislikeBtn)
+
+
+
+        contentTxt.text = data.content
+
 
         val data = mList[Transliterator.Position]
 
-        contentTxt.text = data.content
 
         writerNickNameTxt.text = data.user.nickName
         contentTxt.text = data.content
+
+        replyBtn.text = "답글 : ${data.replyCount}"
+        likeBtn.text = "좋아요 : ${data.likeCount}"
+        dislikeBtn.text = "실어요 : ${data.dislikeCount}"
 
         return row
 
