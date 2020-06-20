f2e12a3204e7247a04e5c866a47d3b75406c19e0
ShinYongSUng <newxm@naver.com>
2020-06-20 14:46:16 +0900

    회색 테두리 사각형 XML 추가

--- app/src/main/java/my/shin/apipractice_20200613/ViewTopicDetailActivity.kt
+++ app/src/main/java/my/shin/apipractice_20200613/ViewTopicDetailActivity.kt
@@ -16,13 +16,12 @@ import org.json.JSONObject
 class ViewTopicDetailActivity : BaseAcitivity() {
 
 
-
-//    화면에 넘겨준 주제 id값을 저장할 변수
+    //    화면에 넘겨준 주제 id값을 저장할 변수
     var mTopicId = -1
 
-    lateinit var mTopic : Topic
+    lateinit var mTopic: Topic
 
-    lateinit var mReplyAdapter : ReplyAdapter
+    lateinit var mReplyAdapter: ReplyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +29,6 @@ class ViewTopicDetailActivity : BaseAcitivity() {
         setupevents()
         setvalues()
     }
-
 
 
     override fun setupevents() {
@@ -41,117 +39,125 @@ class ViewTopicDetailActivity : BaseAcitivity() {
                 val myIntent = Intent(mContext, EditReplyActivity::class.java)
                 myIntent.putExtra("TopicTitle", mTopic.title)
                 myIntent.putExtra("selectedSideTitle", it.title)
-                myIntent.putExtra("")
+                myIntent.putExtra("topicId", mTopicId)
                 startActivity(myIntent)
             }.let {
                 if (it == null) {
                     //                mySelectedSide가 null인 경우 = > 투표를 아직 안한 경우
-                    Toast.makeText(mContext, "맘에 드는 진영을 선택해야 의견을 남길 수 있습니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(mContext, "맘에 드는 진영을 선택해야 의견을 남길 수 있습니다.", Toast.LENGTH_SHORT)
+                        .show()
                 }
 
             }
 
-
         }
 
         voteToFirstBtn.setOnClickListener {
-            ServerUtil.postRequestVote(mContext,mTopic.sides[0].id, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(json: JSONObject) {
-                }
-            })
+            ServerUtil.postRequestVote(
+                mContext,
+                mTopic.sides[0].id,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+                    }
+                })
         }
         voteToSecondBtn.setOnClickListener {
-            ServerUtil.postRequestVote(mContext,mTopic.sides[1].id, object : ServerUtil.JsonResponseHandler{
-                override fun onResponse(json: JSONObject) {
+            ServerUtil.postRequestVote(
+                mContext,
+                mTopic.sides[1].id,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
 
-                    val code = json.getInt("code")
+                        val code = json.getInt("code")
 
-                    if (code == 200) {
-                        runOnUiThread {
-                            Toast.makeText(mContext, "참여해주셔서 감사합니다.", Toast.LENGTH_SHORT).show()
-                       }
-                    }
+                        if (code == 200) {
+                            runOnUiThread {
+                                Toast.makeText(mContext, "참여해주셔서 감사합니다.", Toast.LENGTH_SHORT).show()
+                            }
+                        } else {
+                            val message = json.getString("message")
+                            runOnUiThread {
+                                Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                            }
 
-                    else {
-                        val message = json.getString("message")
-                        runOnUiThread {
-                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
                         }
-
-
-
                     }
-                }
-            })
+                })
         }
 
 
     }
 
     override fun setvalues() {
-        mTopicId = intent.getIntExtra("topic_id",-1)
+        mTopicId = intent.getIntExtra("topic_id", -1)
 
         if (mTopicId == -1) {
 //            주제 id 가 -1로 남아있다 : topic_id 첨부가 제대로 되지 않았다.
-            Toast.makeText(mContext,"잘못된 접근입니다.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(mContext, "잘못된 접근입니다.", Toast.LENGTH_SHORT).show()
 //            코드 추가 진행을 막자.(강제종료)
             return
 
         }
 
         Log.d("넘겨받은 주제 id", mTopicId.toString())
-        
+
 //        넘겨 받은 id값으로 서버에서 주제의 상세 진행 상황 받아오기
 //        onCreate의 하위 기능인 setvalues에서는 서버 통신 x
         // get getTopicdatFromSever()
 //        자동새로고침 구현 사례
-        
-       Override fun onResume() {
-            super.onResume()
-            getTopicDataFromSever()
-            
-        }
+    }
 
-       fun getTopicdatFromSever(){
-           ServerUtil.getRequestTopicDetail(mContext,mTopicId,object : ServerUtil.JsonResponseHandler{
-               override fun onResponse(json: JSONObject) {
+    override fun onResume() {
+        super.onResume()
+        getTopicdatFromSever()
+    }
 
-                   val code = json.getInt("code")
 
-                   if (code == 200){
-                       val data = json.getJSONObject("data")
-                       val topic = data.getJSONObject("topic")
+    fun getTopicdatFromSever() {
+        ServerUtil.getRequestTopicDetail(
+            mContext,
+            mTopicId,
+            object : ServerUtil.JsonResponseHandler {
+
+                override fun onResponse(json: JSONObject) {
+
+                    val code = json.getInt("code")
+
+                    if (code == 200) {
+                        val data = json.getJSONObject("data")
+                        val topic = data.getJSONObject("topic")
 
 //                    멤버변수 mtopic에 서버에서 내려준 내용을 피싱
-                       mTopic = Topic.getTopicFromJson(topic)
+                        mTopic = Topic.getTopicFromJson(topic)
 
-                       runOnUiThread {
+                        runOnUiThread {
 //                    받아온 주제의 제목을 화면에 표시
-                           topicTitleTxt.text = mTopic.title
+                            topicTitleTxt.text = mTopic.title
 
-                           Glide.with(mContext).load(mTopic.imageUrl).into(topicImg)
+                            Glide.with(mContext).load(mTopic.imageUrl).into(topicImg)
 
 //                        선택 진영 정보도 출력
-                           firstSideTxt.text = mTopic.sides[0].title
-                           secondSideTxt.text = mTopic.sides[1].title
+                            firstSideTxt.text = mTopic.sides[0].title
+                            secondSideTxt.text = mTopic.sides[1].title
 
 //                        투표 현황도 파싱 된 데이터를 같이 사용.
 
-                           firstSideVoteCountTxt.text = "${mTopic.sides[0].voteCount}표"
-                           SecondSideVoteCountTxt.text = "${mTopic.sides[1].voteCount}표"
+                            firstSideVoteCountTxt.text = "${mTopic.sides[0].voteCount}표"
+                            SecondSideVoteCountTxt.text = "${mTopic.sides[1].voteCount}표"
 
-                           mReplyAdapter = ReplyAdapter(mContext, R.layout.topic_list_item,mTopic.replies)
-                           replyListView.adapter = mReplyAdapter
-                       }
+                            mReplyAdapter =
+                                ReplyAdapter(mContext, R.layout.topic_list_item, mTopic.replies)
+                            replyListView.adapter = mReplyAdapter
+                        }
 
-                   }
+                    }
 
-               }
+                }
 
-           })
+            })
 
 
-       }
     }
 
 
