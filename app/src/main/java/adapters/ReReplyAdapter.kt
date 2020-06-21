9681eee8636466f25661ad4fb1cd38c5d5085b31
ShinYongSUng <newxm@naver.com>
2020-06-21 11:50:35 +0900

    실제 좋아요 / 싫어요 이벤트 처리  => API가 의견 좋아요 / 싫어요와 완전히 동일함.

--- app/src/main/java/adapters/ReReplyAdapter.kt
+++ app/src/main/java/adapters/ReReplyAdapter.kt
@@ -18,9 +18,9 @@ import my.shin.apipractice_20200613.ViewReplyDetailActivity
 import my.shin.utils.ServerUtil
 import org.json.JSONObject
 
-class ReReplyAdapter(val mContext:Context, val resId:Int, val mList:List<TopicReply>):
+class ReReplyAdapter(val mContext: Context, val resId: Int, val mList: List<TopicReply>) :
 
-    ArrayAdapter <TopicReply>(mContext,resId,mList) {
+    ArrayAdapter<TopicReply>(mContext, resId, mList) {
 
     val inf = LayoutInflater.from(mContext)
 
@@ -58,28 +58,86 @@ class ReReplyAdapter(val mContext:Context, val resId:Int, val mList:List<TopicRe
 
 //        내 좋아요 여부에 따라 모양 변경
 
-        if (data.isMyLike){
+        if (data.isMyLike) {
             likeBtn.setBackgroundResource(R.drawable.red_border_box)
             dislikeBtn.setBackgroundResource(R.drawable.gray_border_box)
 
             likeBtn.setTextColor(mContext.resources.getColor(R.color.red))
             dislikeBtn.setTextColor(mContext.resources.getColor(R.color.blue))
-        }
-
-        else if (data.isMyDislike){
+        } else if (data.isMyDislike) {
             likeBtn.setBackgroundResource(R.drawable.gray_border_box)
             dislikeBtn.setBackgroundResource(R.drawable.blue_border_box)
 
             likeBtn.setTextColor(mContext.resources.getColor(R.color.darkGray))
             dislikeBtn.setTextColor(mContext.resources.getColor(R.color.blue))
-        }
-        else {
+        } else {
             likeBtn.setBackgroundResource(R.drawable.gray_border_box)
             dislikeBtn.setBackgroundResource(R.drawable.gray_border_box)
 
             likeBtn.setTextColor(mContext.resources.getColor(R.color.darkGray))
             dislikeBtn.setTextColor(mContext.resources.getColor(R.color.darkGray))
         }
+
+        likeBtn.setOnClickListener {
+//        좋아요 API 호출 => 좋아요 누르기 / 취소 처리
+//        화면에 변경된 좋아요 / 싫어요 갯수 반영 (응용)
+
+            ServerUtil.postRequestReplyLikeOrDislike(
+                mContext,
+                data.id,
+                true,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+//                    화면에 변경된 좋아요 / 싫어요 방법
+                        val dataObj = json.getJSONObject("data")
+                        val reply = dataObj.getJSONObject("reply")
+
+//                    목록에서 꺼낸 data변수의 객체를 통째로 바꾸는건 불가능.
+//                    var로 바꿔서 통쨰로 바꿔도 => 목록에는 반영되지 않음.
+//                    data = TopicReply.getTopicReplyFromJson()
+//                    목록에서 꺼낸 data 변수의 좋아요 갯수 / 싫어요 갯수를 직접 변경
+                        data.likeCount = reply.getInt("like_count")
+                        data.dislikeCount = reply.getInt("dislike_count")
+
+                        data.isMyLike = reply.getBoolean("my_like")
+                        data.isMyDislike = reply.getBoolean("my_dislike")
+
+//                    목록의 내용을 일부 변경 => 반영하려면
+//                    어댑터.notifyDataSetChanged() 실행 필요함
+//                    이미 어댑터 내부에 있는 상황 => 곧바로 notifyDataSetChanged() 실행 가능
+
+//                    RunOnUiThread로 처리 필요 => 어댑터내부에선 사용 불가.
+//                    대체재 : Handler(Looper.getMainLooper()).post (UI 쓰레드 접근하는 다른 방법)
+                        Handler(Looper.getMainLooper()).post {
+                            notifyDataSetChanged()
+                        }
+                    }
+                })
+        }
+
+        dislikeBtn.setOnClickListener {
+            ServerUtil.postRequestReplyLikeOrDislike(
+                mContext,
+                data.id,
+                false,
+                object : ServerUtil.JsonResponseHandler {
+                    override fun onResponse(json: JSONObject) {
+                        val dataObj = json.getJSONObject("data")
+                        val reply = dataObj.getJSONObject("reply")
+
+
+                        data.likeCount = reply.getInt("like_count")
+                        data.dislikeCount = reply.getInt("dislike_count")
+                        data.isMyLike = reply.getBoolean("my_like")
+                        data.isMyDislike = reply.getBoolean("my_dislike")
+
+                        Handler(Looper.getMainLooper()).post {
+                            notifyDataSetChanged()
+                        }
+                    }
+                })
+        }
+
 
         return row
     }
